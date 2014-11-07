63c663b156d9f0f68b08874b1cea82afca91e5c4
Thomas Nabord <thomas.nabord@prestashop.com>
2014-11-07 16:13:24 +0100

    Add french translations

---
+++ fr.php
@@ -0,0 +1,12 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{gadwords}prestashop>gadwords_ef5ad0553c3a21e89c24fc59f267fdb4'] = 'Vous souhaitez être plus visible sur Google et attirer de nouveaux clients ? Bénéficiez de votre code promo 75€ offerts sur Google AdWords !';
+$_MODULE['<{gadwords}prestashop>gadwords_9a8ea889063b570f39791f8670d1b71b'] = 'Diffusez votre publicité aux internautes au moment où ils cherchent vos produits ou vos services. Google et PrestaShop triplent votre investissement publicitaire en vous offrant 75€ une fois vos premiers 25€ dépensés* !';
+$_MODULE['<{gadwords}prestashop>gadwords_cee8a737fd311b8eb88a01e64e283636'] = 'Entrez votre code promotionnel une fois vos informations de facturation saisies et nous ajouterons automatiquement votre avoir dès €25 dépensés*.';
+$_MODULE['<{gadwords}prestashop>gadwords_40c2846680e4c15973eb7363e1053b2f'] = 'Vous avez des questions ? Contactez le 0 805 981 042 et bénéficiez gratuitement de l\'aide d\'un expert Google AdWords pour créer votre première campagne et vous expliquer comment tirer le meilleur parti d\'AdWords.';
+$_MODULE['<{gadwords}prestashop>gadwords_c391b2a443c8cf6ac0650abb19bc60e2'] = 'Votre code promo Google AdWords pour votre boutique est';
+$_MODULE['<{gadwords}prestashop>gadwords_74bdcf82592645b70e8172ec2d68f8eb'] = 'http://www.google.com/intl/fr/ads/get/prestashop75/index.html';
+$_MODULE['<{gadwords}prestashop>gadwords_310ea748429c48440caefe153ff77855'] = 'Commencez votre campagne maintenant en utilisant votre code promo';
+$_MODULE['<{gadwords}prestashop>gadwords_e64a391c8b0d9015df793a179fb12938'] = 'offre soumise à conditions';
