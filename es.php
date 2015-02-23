fd71a03695382163606e4eecf69fce4fc61a74ff
Thomas Nabord <thomas.nabord@prestashop.com>
2015-02-23 18:36:46 +0100

    First version of es and it translations

---
+++ es.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{gadwords}prestashop>gadwords_77f0fe9b5917f3651715b0d38db936ad'] = 'Google AdWords';
+$_MODULE['<{gadwords}prestashop>gadwords_26700df78ce041ff7dc0506489b273f6'] = 'Muestra tu anuncio a las personas en el momento en que están buscando lo que tu negocio ofrece. Google y PrestaShop triplican tu inversión publicitaria ofreciendo € 75 desde 25 € gastados*!';
+$_MODULE['<{gadwords}prestashop>gadwords_9b22559aafb75ab90687d7b12cc85973'] = 'Añade tu código promocional de Prestashop en las preferencias de facturación y conseguirás tu crédito publicitario después de haber gastado 25€.*';
+$_MODULE['<{gadwords}prestashop>gadwords_2b456146ad32d5cdee08f1e844346e68'] = '¿Tienes preguntas? Llama al 900 814 546 y un experto en Google AdWords te ayudará con tu primera campaña y te explicará cómo sacar el máximo partido de AdWords.';
+$_MODULE['<{gadwords}prestashop>gadwords_c391b2a443c8cf6ac0650abb19bc60e2'] = 'El código promocional Google AdWords para tu tienda es:';
+$_MODULE['<{gadwords}prestashop>gadwords_310ea748429c48440caefe153ff77855'] = 'Empieza tu campaña ahora con tu código promocional.';
+$_MODULE['<{gadwords}prestashop>gadwords_e64a391c8b0d9015df793a179fb12938'] = '*Se aplican los términos y condiciones.';
---
+++ it.php
@@ -0,0 +1,12 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{gadwords}prestashop>gadwords_77f0fe9b5917f3651715b0d38db936ad'] = 'Google AdWords';
+$_MODULE['<{gadwords}prestashop>gadwords_4bfb064ad861b9d2af3a1cdef6af1bae'] = '//www.youtube.com/embed/GNMZgum0f9g?rel=0&controls=0&showinfo=0';
+$_MODULE['<{gadwords}prestashop>gadwords_26700df78ce041ff7dc0506489b273f6'] = 'Mostra il tuo annuncio a potenziali clienti nel momento stesso in cui la ricerca di ciò che offrite su Google . Google e PrestaShop triplicano il tuo budget pubblicitario offrendoti 75€ se spendi 25€!';
+$_MODULE['<{gadwords}prestashop>gadwords_9b22559aafb75ab90687d7b12cc85973'] = 'Aggiungi il tuo codice promozionale da Prestashop dopo aver inserito i dati di fatturazione . Abbiamo poi accrediterà automaticamente il vostro conto quando si spendono un credito minimo*.';
+$_MODULE['<{gadwords}prestashop>gadwords_2b456146ad32d5cdee08f1e844346e68'] = 'Hai delle domande ? Chiamate 0800 169 0489 e un esperto di Google AdWords vi aiuterà a costruire la tua prima campagna e offrire consigli su come ottenere il massimo da AdWords.';
+$_MODULE['<{gadwords}prestashop>gadwords_c391b2a443c8cf6ac0650abb19bc60e2'] = 'Il codice promozionale di Google AdWords è :';
+$_MODULE['<{gadwords}prestashop>gadwords_310ea748429c48440caefe153ff77855'] = 'Inizia la tua campagna ora con il vostro codice promozionale';
+$_MODULE['<{gadwords}prestashop>gadwords_e64a391c8b0d9015df793a179fb12938'] = 'Termini e condizioni.';
